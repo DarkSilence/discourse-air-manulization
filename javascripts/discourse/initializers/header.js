6bf2520f6e7689bc350d9b784cc997d549df5a69
DarkSilence <roma@paulov.com>
2024-09-26 23:32:40 +0200

    Menu p1

--- javascripts/discourse/initializers/header.js
+++ javascripts/discourse/initializers/header.js
@@ -7,15 +7,9 @@ export default {
   initialize() {
     withPluginApi("0.8.14", (api) => {
         console.log(0);
-      api.decorateWidget('home-logo:after', helper => {
-          console.log(1);
-          return helper.h('li.header-dropdown-toggle', [
-              helper.h('a.icon', {
-                  href:'/docs',
-                  title: 'Document'
-              }, iconNode("book")),
-          ]);
-      });
+      api.headerIcons.add("some-unique-name", <template>
+    <li><DButton class="icon btn-flat" @href="/u" @icon="address-book" /></li>
+  </template>);
 
       api.onPageChange(() => {
         // const locale = I18n.currentLocale(); // Get the user's locale
