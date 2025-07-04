bf14a3911ab39322756f9fd64fa3305cddfb75c8
DarkSilence <roma@paulov.com>
2025-07-04 21:14:39 +0200

    Compatibility fix

--- javascripts/discourse/initializers/init-topic-excerpts.js
+++
@@ -1,8 +0,0 @@
-import { apiInitializer } from "discourse/lib/api";
-
-export default apiInitializer("1.0.0", (api) => {
-  api.modifyClass("component:topic-list-item", {
-    pluginId: "discourse-manulization",
-    expandPinned: true,
-  });
-});
